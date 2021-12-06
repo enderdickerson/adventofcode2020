efc5f80e9966e7bd2232497fdd631dab18faa0b6
Andrew Dickerson <olah89@gmail.com>
2021-12-05 19:30:14 -0700

    feat(day3): adds part 2 solution

--- src/main/scala/day3/day3.scala
+++ src/main/scala/day3/day3.scala
@@ -3,12 +3,29 @@ package day3
 import scala.io.Source
 
 object day3 extends App {
-  val hitTrees = (pattern: List[String]) =>
+  case class adventure(treesHit: Int, x: Int, y: Int)
+
+  val hitTrees = (pattern: List[String], goRight: Int, goDown: Int) =>
     pattern
-      .foldLeft((0,0))((acc, curr) => {
-        (acc._1 + (if (curr(acc._2 % pattern(0).length) == '#') 1 else 0), acc._2 + 3)
+      .foldLeft(adventure(0,0,0))((acc, curr) => {
+        if (acc.y % goDown == 0) {
+          val treeDamage = if (curr(acc.x % pattern(0).length) == '#') 1 else 0
+          adventure(acc.treesHit + treeDamage, acc.x + goRight, acc.y + 1)
+        }
+        else adventure(acc.treesHit, acc.x, acc.y + 1)
       })
       ._1
 
-  println(hitTrees(Source.fromFile("src/main/scala/day3/day3.txt").getLines().toList))
+  val checkTreePaths = (pattern: List[String]) => {
+    List(
+      hitTrees(pattern, 1, 1),
+      hitTrees(pattern, 3, 1),
+      hitTrees(pattern, 5, 1),
+      hitTrees(pattern, 7, 1),
+      hitTrees(pattern, 1, 2)
+    ).map(x => BigInt(x)).product
+  }
+
+//  println(hitTrees(Source.fromFile("src/main/scala/day3/day3.txt").getLines().toList, 3, 1))
+  println(checkTreePaths(Source.fromFile("src/main/scala/day3/day3.txt").getLines().toList))
 }
