b420a6120128cd61d48ce74e74e2d1a9ef1aaeea
Andrew Dickerson <olah89@gmail.com>
2021-12-05 18:40:21 -0700

    feat(day3): optimize fill size

--- src/main/scala/day3/day3.scala
+++ src/main/scala/day3/day3.scala
@@ -5,9 +5,10 @@ import scala.io.Source
 object day3 extends App {
   val hitTrees = (pattern: List[String]) =>
     pattern
-      .map(x => List.fill(6)(x).mkString(""))
-      .foldLeft((0,0))((acc, curr) => (acc._1 + (if (curr(acc._2) == '#') 1 else 0), acc._2 + 3))
+      .foldLeft((0,0))((acc, curr) => {
+        (acc._1 + (if (curr(acc._2 % pattern(0).length) == '#') 1 else 0), acc._2 + 3)
+      })
       ._1
 
-  println(hitTrees(Source.fromFile("src/main/scala/day3/day3test.txt").getLines().toList))
+  println(hitTrees(Source.fromFile("src/main/scala/day3/day3.txt").getLines().toList))
 }
